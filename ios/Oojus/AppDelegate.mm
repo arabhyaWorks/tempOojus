4da98c3f069098cc67b1ca3519257e1f82f89c37
R Animesh <ranimesh@192.168.1.3>
2024-07-31 10:01:57 +0530

    configured the project react native fonts and firedatabase is left

--- ios/Oojus/AppDelegate.mm
+++ ios/Oojus/AppDelegate.mm
@@ -1,11 +1,13 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyB4jRJ25sj_mLjuPoL0v22ocrE-kS-PRZw"]; // add this line using the api key obtained from Google Console
   self.moduleName = @"Oojus";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
